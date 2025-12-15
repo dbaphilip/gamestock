47c3516529de33d336bcb5e725db60b55a701155
agabyte <agabyte@agabyte.com>
2025-12-15 13:34:56 +0100

    add responsive l/o

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,16 @@ export default function Home() {
   return (
     <div className="container">
       <Navbar />
+
+      <div className="row mt-5">
+        <div className="col-4 d-none d-md-block">
+          <aside style={{ backgroundColor: "red", height: "300px" }}></aside>
+        </div>
+
+        <div className="col-md-8">
+          <main style={{ backgroundColor: "yellow", height: "300px" }}></main>
+        </div>
+      </div>
     </div>
   );
 }
