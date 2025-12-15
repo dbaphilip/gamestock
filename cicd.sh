d090f6440abe8a348a4ea6d0f8956458e9133bbd
agabyte <agabyte@agabyte.com>
2025-12-15 13:28:00 +0100

    NPM CI -> to NPM I

--- cicd.sh
+++ cicd.sh
@@ -4,7 +4,7 @@ cd /root/gamestock
 git pull
 source /root/.nvm/nvm.sh
 
-/root/.nvm/versions/node/v24.12.0/bin/npm ci
+/root/.nvm/versions/node/v24.12.0/bin/npm i
 
 /root/.nvm/versions/node/v24.12.0/bin/npm run build
 
